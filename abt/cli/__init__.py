074fcf34aa3a7231a4f2e3067e0d494e09093236
kamikat <kamikat@banana.moe>
2017-06-14 16:29:09 +0800

    Add cli `add` command

--- abt/cli/__init__.py
+++ abt/cli/__init__.py
@@ -1,4 +1,20 @@
+import argparse
 from sys import argv
 from os.path import basename, splitext
 
 progname = "abt %s" % splitext(basename(argv[0]))[0]
+
+class StoreToDict(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        d = getattr(namespace, self.dest)
+        for opt in values:
+            k,v = opt.split("=", 1)
+            k = k.lstrip("-")
+            d[k] = v
+        setattr(namespace, self.dest, d)
+
+def parse_options(args):
+    parser = argparse.ArgumentParser(prefix_chars=' ')
+    parser.add_argument("options", nargs="*", action=StoreToDict, default=dict())
+    return parser.parse_args(args).options
+
---
+++ abt/cli/add.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+
+"""
+Add BitTorrent download task
+"""
+
+import argparse
+import abt.cli as cli
+import abt.rpc_client as client
+import base64
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog=cli.progname, description=__doc__.strip())
+    parser.add_argument('torrent', nargs='?', action='store', help="path to torrent file")
+    parser.add_argument('--uri', action='store', help="load torrent file from uri")
+    args, extra = parser.parse_known_args()
+    options = cli.parse_options(extra)
+
+    if (not args.torrent) == (not args.uri):
+        parser.error("Exactly one of torrent file path or --uri is expected.")
+
+    aria2, _ = client.connect()
+
+    if args.torrent:
+        torrent_data = open(args.torrent, 'rb').read()
+        torrent_b64 = base64.b64encode
+        print aria2.addTorrent(torrent_b64, [], options)
+    else:
+        options['follow-torrent'] = 'true'
+        print aria2.addUri([args.uri], options)
+
