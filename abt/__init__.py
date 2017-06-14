2cc3e1bc9ba7135219a4aa39d8a8b9e71832793e
kamikat <kamikat@banana.moe>
2017-06-14 17:34:31 +0800

    Refactor

--- abt/__init__.py
+++ abt/__init__.py
@@ -1,1 +1,1 @@
-from __main__ import main
+from abt.cli import main
--- abt/cli/__init__.py
+++ abt/cli/__init__.py
@@ -1,8 +1,31 @@
+import sys
+import subprocess
 import argparse
-from sys import argv
-from os.path import basename, splitext
+from os import path, listdir
+from imp import load_source
 
-progname = "abt %s" % splitext(basename(argv[0]))[0]
+progname = path.splitext(path.basename(sys.argv[0]))[0]
+progname = "abt %s" % progname if progname != "abt" else "abt"
+cli_path = path.dirname(__file__)
+
+def main():
+    if len(sys.argv) > 1:
+        command = path.join(cli_path, "%s.py" % sys.argv[1])
+        if path.exists(command):
+            subprocess.call([sys.executable, command] + sys.argv[2:])
+            return
+    print "usage: %s <command> [<args>]\n" % progname
+    print "abt is a BitTorrent management workflow.\n"
+    print "List of Commands:"
+    for command_source in listdir(cli_path):
+        if command_source.endswith('.pyc') or command_source.startswith('__'):
+            continue
+        command = command_source.split('.')[0]
+        command_file = path.join(cli_path, command_source)
+        command_module = load_source(command, command_file)
+        description = command_module.__doc__.strip().replace('\n', '\n' + ' ' * 20) if command_module.__doc__ else ""
+        print "   %s %s" % ("{0: <16}".format(command), description)
+    print
 
 class StoreToDict(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
